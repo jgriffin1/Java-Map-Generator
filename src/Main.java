2f7eaf972087e56cae8f9e89a3c30e5a9ef18bd5
Josh Griffin <jgriffin1@umary.edu>
2020-04-14 21:02:57 -0500

    fix

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        //Map testmap = new Map("HIGH_LORD_VOLTRON");
-        Map testmap = new Map(String.valueOf(System.currentTimeMillis()));
-    }
-}
